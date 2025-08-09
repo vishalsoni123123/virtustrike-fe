014ca19a04debeca92693bbe9fa99fb3a212c2b6
parag477 <paragagrawal477@gmail.com>
2025-08-09 17:46:14 +0530

    fix: solved vercel errors

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/service/Auth-Service';
+import { AuthService } from 'src/app/service/auth-service';
 
 @Component({
   selector: 'app-navbar',
