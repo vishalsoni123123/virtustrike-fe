9bd21aeb6a97c4b583c950b58a260770f26893f9
VISHAL SONI <132942287+vishalsoni123123@users.noreply.github.com>
2025-12-17 19:01:45 +0530

    Update backendBaseUrl for production environment
    reversal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
-    production: true,
-    backendBaseUrl: '/virtustrike-management-api-local',
-   
-    //backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
+    production: true,   
+    backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
     s3BaseUrl: 'https://virtustrike-assets.s3.ap-south-1.amazonaws.com'
 };
