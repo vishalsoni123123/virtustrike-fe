da84661d90b0aeab7dead9388ed9beca6fb83d53
VISHAL SONI <132942287+vishalsoni123123@users.noreply.github.com>
2025-12-17 18:57:50 +0530

    Change backendBaseUrl to relative path
    Updated backendBaseUrl to use a relative path.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
     production: false,
-    backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
+    backendBaseUrl: '/virtustrike-management-api-local',
+
+    //backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
     s3BaseUrl: 'https://virtustrike-assets.s3.ap-south-1.amazonaws.com'
 };
 
