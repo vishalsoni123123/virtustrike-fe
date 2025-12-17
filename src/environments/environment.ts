31ac5ee1fba0f3c4ca9eff16e0d9c68faecd2b7e
VISHAL SONI <132942287+vishalsoni123123@users.noreply.github.com>
2025-12-17 19:11:22 +0530

    Update backendBaseUrl for environment configuration
    api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
     production: false,
-    backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
+ //   backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
+    backendBaseUrl: 'https://api.virtustrike.com',
     s3BaseUrl: 'https://virtustrike-assets.s3.ap-south-1.amazonaws.com'
 };
 
