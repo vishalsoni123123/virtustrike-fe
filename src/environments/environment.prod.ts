70b7dd2f17553377af31509c9090e97ff7b61d78
VISHAL SONI <132942287+vishalsoni123123@users.noreply.github.com>
2025-12-17 18:38:36 +0530

    Update environment.prod.ts
    false to true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     backendBaseUrl: 'https://virtustrike.com/virtustrike-management-api-local',
     s3BaseUrl: 'https://virtustrike-assets.s3.ap-south-1.amazonaws.com'
 };
