2eddab919ceda12f231f6f766c7e6a9af3083443
VISHAL SONI <132942287+vishalsoni123123@users.noreply.github.com>
2025-12-17 21:00:26 +0530

    Update url-service.ts
    1
    api

--- src/app/service/url-service.ts
+++ src/app/service/url-service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class UrlService {
 
-  private readonly baseUrl = 'https://virtustrike.com/virtustrike-management-api-local';
+  private readonly baseUrl = 'https://api.virtustrike.com/virtustrike-management-api-local';
 
   public getBaseUrl(): string {
     return this.baseUrl;
